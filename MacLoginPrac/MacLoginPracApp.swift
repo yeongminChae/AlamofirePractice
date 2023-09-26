05a4937ca1475ad9d62505abe0a133936587a211
Yeongmin_Chae <codudals98@naver.com>
2023-09-26 18:01:08 +0900

    Initial Commit

---
+++ MacLoginPrac/MacLoginPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLoginPracApp.swift
+//  MacLoginPrac
+//
+//  Created by 채영민 on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MacLoginPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
